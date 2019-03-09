7cbc051987001a95e057414638e2c8b30ee6e70c
Jose Claudio <jcsj.ti@gmail.com>
2019-03-08 21:31:25 -0300

    Iniciando projeto

---
+++ src/main/java/dev/joseclaudio/SpringJpaQueryDslApplication.java
@@ -0,0 +1,13 @@
+package dev.joseclaudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaQueryDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaQueryDslApplication.class, args);
+	}
+
+}
